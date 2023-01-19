420f7da8c40dfff1868894b8e59675094db4ac7b
cynka02 <darnok.ginneh@gmail.com>
2023-01-19 22:09:28 +0100

    Better image printing v2

--- src/Data/LabeledImage.java
+++ src/Data/LabeledImage.java
@@ -22,23 +22,9 @@ public class LabeledImage {
 
         System.out.println("Label: " + label);
 
-        for(int i =0; i < data.length; i++){
-            for(int j =0; j < data[0].length; j++){
-                System.out.print(data[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    public void prettyPrintData(){
-
-        System.out.println("Label: " + label);
-
-        for(int i =0; i < data.length; i++){
-            for(int j =0; j < data[0].length; j++){
-
-                // to be changed after updating ImageConverter to represent image as numbers from 0 to 256
-                System.out.print(" .^*$@#".charAt((int) Math.round((Math.round(data[i][j]*-1)/16777216.0)*6)) + " ");
+        for(int i=0; i < data.length; i++){
+            for(int j=0; j < data[0].length; j++){
+                System.out.print(" .^*$@#".charAt((int) Math.round((Math.round(data[i][j])/256.0)*6)) + " ");
             }
             System.out.println();
         }
